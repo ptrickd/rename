550fd1ab3a0c7eba9e38134f2df15239d0bb8131
ptrickd <patrickdion8@gmail.com>
2023-10-17 15:43:13 -0600

    find the files with arguments

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-while getopts p:f:r: flag
+while getopts p:f:r:t: flag
 
 
 
@@ -9,6 +9,7 @@ do
         p) pathname=${OPTARG};;
         f) find=${OPTARG};;
         r) replace=${OPTARG};;
+        t) typ=${OPTARG};;
     esac
 done
 
@@ -21,50 +22,18 @@ elif [ -z "$pathname" ]; then
 elif [ -z "$replace" ]; then
    echo 'Need a argument replace'
    exit 1
+elif [ -z "$typ" ]; then
+   echo 'Need a argument type'
+   exit 1
 fi
 
-echo "Pathname: $pathname";
-echo "Find: $find";
-echo "Replace with: $replace";
 
-# (cd $pathname; array=( $( find "$find"* ) ))
+array=( $( find $pathname -maxdepth 1 -name "*${typ}" | grep "$find") )
 
-# echo -n "Path to the folders:"
-# read path
-# cd path
-
-#echo -n "Part of the filename to replace:"
-#read files
-
-#echo -n "Replace with:"
-#read replacing
-
-#echo $PATH
-#echo $path
-
-#newPath=$PATH:"/"+$path
-
-#echo $files
-#echo $replacing
-# echo $path
-# echo "$@"
-
-# absolutePath="false"
-
-# if [[ $path == *"~"  ]]; then
-#    absolutePath="true"
-# fi 
-# #cd $path
-# if [[ $absolutePath == "true" ]]; then
-#    echo "its true"
-# else
-#    echo "its false"
-# fi
-#command gives us the an array of files
-#array=( $( find "$path"* ) )
 
 #print the length of the array
 echo "${#array[@]}"
 
-#value at index 0
-#echo "${array[0]}"
+for i in "${array[@]}"; do
+	echo $i 
+done
