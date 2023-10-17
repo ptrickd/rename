f3a3939e733e9c09eb9f6574f9d0fe6707a9f7b1
ptrickd <patrickdion8@gmail.com>
2023-10-17 15:58:58 -0600

    replace part of the filename

--- rename.sh
+++ rename.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+#gets the arguments
 while getopts p:f:r:t: flag
 
-
-
+# assign the arguments 
 do
     case "${flag}" in
         p) pathname=${OPTARG};;
@@ -13,6 +13,7 @@ do
     esac
 done
 
+# if arguments missing, stop ths script
 if [ -z "$find" ]; then
    echo 'Need a argument find'
    exit 1
@@ -27,13 +28,15 @@ elif [ -z "$typ" ]; then
    exit 1
 fi
 
-
+# store all the filename found in an array
 array=( $( find $pathname -maxdepth 1 -name "*${typ}" | grep "$find") )
 
 
 #print the length of the array
-echo "${#array[@]}"
+echo "${#array[@]} files found"
 
-for i in "${array[@]}"; do
-	echo $i 
+# loop through the array replacing part of the text
+for file in "${array[@]}"; do
+   mv -- "$file" "${file//${find}/${replace}}"  
 done
+
